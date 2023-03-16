4ec80a6f4b2de1d2357de3ed40e743b4c26dc6c8
Lukkas98 <lucaspalma988@gmail.com>
2023-03-16 10:57:00 -0300

    deploy

--- client/src/index.js
+++ client/src/index.js
@@ -5,12 +5,11 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from 'react-router-dom'
 import axios from "axios";
+import  store  from './redux/store'
+import { Provider } from 'react-redux'
 
 // axios.defaults.baseURL = "http://localhost:3001"
 axios.defaults.baseURL = "https://prueba-deploypi-production.up.railway.app"
-
-import  store  from './redux/store'
-import { Provider } from 'react-redux'
 
 ReactDOM.render(
   <Provider store={store}>
