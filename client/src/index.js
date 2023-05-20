301088cce20f90577c1ee574f7ec85e9c98a6d4d
Lukkas98 <lucaspalma988@gmail.com>
2023-05-20 15:45:45 -0300

    deploy render

--- client/src/index.js
+++ client/src/index.js
@@ -2,14 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from 'react-router-dom'
 import  store  from './redux/store'
 import { Provider } from 'react-redux'
 import axios from "axios"
 
 // axios.defaults.baseURL = "http://localhost:3001"
-axios.defaults.baseURL = "https://prueba-deploypi-production.up.railway.app"
+axios.defaults.baseURL = "https://back-videogames-fihv.onrender.com/"
 
 ReactDOM.render(
   <Provider store={store}>
@@ -19,8 +18,3 @@ ReactDOM.render(
   </Provider>,
   document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
