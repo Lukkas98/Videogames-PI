c5401078d61618994b84617df757bc165bede356
Lukkas98 <lucaspalma988@gmail.com>
2023-05-23 17:22:21 -0300

    fix form

--- client/src/components/Form/Form.jsx
+++ client/src/components/Form/Form.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import "./Form.modules.css"
 import validate from "./validation"
@@ -11,35 +11,49 @@ export default function Form(){
 
     //otener todos los generos
     const allGenres = useSelector(state => state.allGenres)
+    const [formComplete, setFormComplete] = useState(false);
     
     const objForState = {
         name:"",
         description:"",
         releaseDate:"",
         rating: "",
-        platforms: "",
+        platforms: [],
         image: "",
         genres: []
     }
     const [gameData, setGameData] = useState({...objForState});
-
     const [ errors, setErrors ] = useState({...objForState});
 
     const handleInputChange = (e)=>{
         setGameData({
             ...gameData,
             [e.target.name] : e.target.value
-        })
-        setErrors((validate(gameData)));
+        })  // setErrors((validate(gameData)));
     }
+    useEffect(() => {
+        setErrors(validate(gameData));
+
+        if (errors.name || errors.description || errors.genres || errors.platforms || errors.rating || errors.releaseDate) {
+            setFormComplete(false);
+        }else{
+            setFormComplete(true);
+        }
+    }, [gameData]);
 
     //estado
     const setPlatforms = (e)=>{
         if (!gameData.platforms.includes(e.target.value)) {
             setGameData({
                 ...gameData,
-                platforms: gameData.platforms.concat(`${e.target.value} `)
+                platforms: [...gameData.platforms, e.target.value]
             })
+        }else{
+            let delPlatform = gameData.platforms.filter( platform => platform !== e.target.value)
+            setGameData({
+                ...gameData,
+                platforms: delPlatform
+            }) 
         }
         e.target.value = ""
     }
@@ -65,16 +79,19 @@ export default function Form(){
     const onSubmit = async (e)=>{
         try {
             e.preventDefault();
-            const {data} = await axios.post("/videogames", gameData);
-            
+            const {data} = await axios.post("/videogames", {
+                ...gameData,
+                platforms: gameData.platforms.join(" ")
+            });
             dispatch(createGame(data));
-            setGameData(objForState)
-            setErrors(objForState)
+            
+            setGameData(objForState);
+            setErrors(objForState);
             setInfo("Videogame Created")
         } catch (error){
             const data = error.response.data.error;
-            setErrors(objForState)
-            setGameData(objForState)
+            setGameData(objForState);
+            setErrors(objForState);
             setInfo(data)
         }
     }
@@ -85,54 +102,55 @@ export default function Form(){
             <div className="containerForm">
                 <h2>Create a VideoGame</h2>
                 <form className="form" onSubmit={onSubmit}>
-                    
-                    <input onChange={handleInputChange} type="text" name="name" placeholder="Name" autoComplete="off" value={gameData.name}/>
-                    
-                    {errors.name && <span>{errors.name}</span>}
-                    
-                    <input onChange={handleInputChange} type="text" name="description" placeholder="description" autoComplete="off" value={gameData.description} />
-                    
-                    {errors.description && <span>{errors.description}</span>}
-                    
-                    <select onChange={setPlatforms}>
-                        <option hidden value="">Platforms</option>
-                        <option value="PC">PC</option>
-                        <option value="XBOX 360">XBOX 360</option>
-                        <option value="XBOX ONE">XBOX ONE</option>
-                        <option value="PLAY STATION">PLAY STATION 1</option>
-                        <option value="PLAY STATION 2">PLAY STATION 2</option>
-                        <option value="PLAY STATION 3">PLAY STATION 3</option>
-                        <option value="PLAY STATION 4">PLAY STATION 4</option>
-                        <option value="PLAY STATION 5">PLAY STATION 5</option>
-                    </select>
-                    {gameData.platforms && <span className="dataSpan" >{gameData.platforms} </span>}
-                    
-                    <input onChange={handleInputChange} type="text" name="image" placeholder="image src" autoComplete="off" value={gameData.image} />
-                    
-                    {errors.image && <span>{errors.image}</span>}
-                    
-                    <input onChange={handleInputChange} type="text" name="releaseDate" placeholder="releaseDate" value={gameData.releaseDate}/>
-                    
-                    {errors.releaseDate && <span>{errors.releaseDate}</span> }
-                    
-                    <input onChange={handleInputChange} type="text" name="rating" placeholder="rating" value={gameData.rating}/>
-                    
-                    {errors.rating && <span>{errors.rating}</span> }
-
-                    
-                    <select onChange={setGenres} defaultValue="Genres" >
-                        <option hidden value="">Genres</option>
-                        {
-                        allGenres.length && allGenres.map((genre, i) => {
-                            return <option key={i}>{genre.name}</option>;
-                        })
-                        }
-                    </select>
-                    
-                    {gameData.genres && <span className="dataSpan">{gameData.genres + " "}</span>}
-                    
+                    <div>
+                        <input style={errors.name ? {border: "1px solid #FB3640"} : {}} onChange={handleInputChange} type="text" name="name" placeholder="Name" autoComplete="off" value={gameData.name}/> 
+                        {errors.name && <span>{errors.name}</span>}
+                    </div>
+                    <div>
+                        <textarea style={errors.description ? {border: "1px solid #FB3640"} : {}} onChange={handleInputChange} type="text" name="description" placeholder="description" autoComplete="off" value={gameData.description}></textarea>
+                        {errors.description && <span>{errors.description}</span>}
+                    </div>
+                    <div>
+                        <select onChange={setPlatforms} style={errors.platforms ? {border: "1px solid #FB3640"} : {}} >
+                            <option hidden value="">Platforms</option>
+                            <option value="PC">PC</option>
+                            <option value="XBOX 360">XBOX 360</option>
+                            <option value="XBOX ONE">XBOX ONE</option>
+                            <option value="PLAY STATION">PLAY STATION 1</option>
+                            <option value="PLAY STATION 2">PLAY STATION 2</option>
+                            <option value="PLAY STATION 3">PLAY STATION 3</option>
+                            <option value="PLAY STATION 4">PLAY STATION 4</option>
+                            <option value="PLAY STATION 5">PLAY STATION 5</option>
+                        </select>
+                        {errors.platforms && <span>{errors.platforms}</span>}
+                        {gameData.platforms && <span className="dataSpan" >{gameData.platforms + " "} </span>}
+                    </div>
+                    <div>
+                        <input onChange={handleInputChange} type="text" name="image" placeholder="image src" autoComplete="off" value={gameData.image} />
+                        {errors.image && <span>{errors.image}</span>}
+                    </div>
+                    <div>
+                        <input style={errors.releaseDate ? {border: "1px solid #FB3640"} : {}} onChange={handleInputChange} type="date" name="releaseDate" placeholder="releaseDate" value={gameData.releaseDate}/>
+                        {errors.releaseDate && <span>{errors.releaseDate}</span> }
+                    </div>
+                    <div>
+                        <input style={errors.rating ? {border: "1px solid #FB3640"} : {}} onChange={handleInputChange} type="text" name="rating" placeholder="rating" value={gameData.rating}/>
+                        {errors.rating && <span>{errors.rating}</span> }
+                    </div>
+                    <div>
+                        <select style={errors.genres ? {border: "1px solid #FB3640"} : {}} onChange={setGenres} defaultValue="Genres" >
+                            <option hidden value="">Genres</option>
+                            {
+                            allGenres.length && allGenres.map((genre, i) => {
+                                return <option key={i}>{genre.name}</option>;
+                            })
+                            }
+                        </select>
+                        {errors.genres && <span>{errors.genres}</span> }
+                        {gameData.genres && <span className="dataSpan">{gameData.genres + " "}</span>}
+                    </div>
                     {
-                        errors.name || errors.description || errors.genres || errors.platforms || errors.rating || errors.releaseDate ?  
+                        !formComplete ?  
                         <button disabled >Please Complete Form</button>
                          : 
                         <button type="submit">Create</button>
--- client/src/components/Form/validation.js
+++ client/src/components/Form/validation.js
@@ -10,15 +10,35 @@ export default function validate(inputs){
 
     if(!inputs.description) errors.description = "A game description is required";
     if(inputs.description.length > 200) errors.description = "The description is very long. should not be longer than 200 characters";
-    
+
     if(!regexDate.test(inputs.releaseDate)) errors.releaseDate = "the format must be YYYY-MM-DD";
-    if(!inputs.releaseDate) errors.releaseDate = "A Date is required";
+    if(!inputs.releaseDate) {
+        errors.releaseDate = "A Date is required";
+    }else{
+        const date = new Date();
+        const year = date.getFullYear();
+        const month = (date.getMonth() + 1);
+        const day = date.getDate();
+
+        const inputDate = inputs.releaseDate.split("-");
+        const inputYear = parseInt(inputDate[0]);
+        const inputMonth = parseInt(inputDate[1]);
+        const inputDay = parseInt(inputDate[2]);
+
+        if (inputYear > year) errors.releaseDate = "The date cannot be greater than the current date";
+        else if(inputYear === year && inputMonth > month ) errors.releaseDate = "The date cannot be greater than the current date";
+        else if(inputYear === year && inputMonth === month && inputDay > day) errors.releaseDate = "The date cannot be greater than the current date";
+    }
 
     if(!regexNum.test(inputs.rating)) errors.rating = "The value must be between 0.00 and 5.00";
     if(!inputs.rating) errors.rating = "A rating is required";
 
     if (!regexImage.test(inputs.image)) errors.image = "URL must be an image (.jpg, .png or .gif)";
     if(inputs.image.length > 200) errors.image = "URL is too long";
+
+    if (!inputs.platforms.length) errors.platforms = "There must be at least 1 platform"
+
+    if (!inputs.genres.length) errors.genres = "There must be at least 1 genre"
 
     return errors;
 }
