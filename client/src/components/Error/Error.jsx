69e0bf4c32ccb3752b1480ad7a3568d32584e77f
Lukkas98 <lucaspalma988@gmail.com>
2023-05-26 17:59:27 -0300

    changed style error

--- client/src/components/Error/Error.jsx
+++ client/src/components/Error/Error.jsx
@@ -4,27 +4,27 @@ import { NavLink } from "react-router-dom";
 export default function Error( {nameError, errorPage} ){
 
     return(
+        <>
+        <NavLink to="/home" className="link linkError">BACK HOME</NavLink>  
         <div className="divError">
             {
                 nameError ? 
                 (
-                    <>
-                        <p>ERROR 404 NOT FOUND</p>
-                        <p>There're no games with name {nameError}</p>
-                    </>
+                    <div className="errorContainer">
+                        <p className="errorText">ERROR 404 NOT FOUND</p>
+                        <p className="errorText">There're no games with name {nameError}</p>
+                    </div>
                 )
-                : !errorPage && <p>ERROR 404 Games not Found</p>
+                : !errorPage && <p className="errorText">ERROR 404 Games not Found</p>
             }
             {
                 errorPage && 
-                <div style={{textAlign:"center"}}>
-                    <NavLink to="/home" className="link" style={{display:"inline-block"}} >BACK HOME</NavLink> 
-                    
-                    <p>ERROR 404</p>
-                    <p>PAGE NOT FOUND</p>
-                    
+                <div className="errorContainer">         
+                    <p className="errorText">ERROR 404</p>
+                    <p className="errorText">PAGE NOT FOUND</p>
                 </div>
             }
         </div>
+        </>
     )
 }
