9c308d8a00706e052e67f66606b03898392865de
Lucas Palma <85300897+Lukkas98@users.noreply.github.com>
2023-11-15 16:39:38 -0300

    Update index.js

--- client/src/index.js
+++ client/src/index.js
@@ -8,7 +8,7 @@ import { Provider } from 'react-redux'
 import axios from "axios"
 
 // axios.defaults.baseURL = "http://localhost:3001"
-axios.defaults.baseURL = "https://back-videogames-fihv.onrender.com/"
+axios.defaults.baseURL = "https://back-videogames2.onrender.com/"
 
 ReactDOM.render(
   <Provider store={store}>
