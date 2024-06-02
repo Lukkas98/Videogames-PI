aeefa781936dd1e660e919b3feecb9d47a71d71b
Lukkas98 <lucaspalma988@gmail.com>
2024-06-02 01:44:31 -0300

    fix backend

--- api/index.js
+++ api/index.js
@@ -17,17 +17,16 @@
 //     =====`-.____`.___ \_____/___.-`___.-'=====
 //                       `=---='
 //     ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-const server = require('./src/app.js');
-const { conn } = require('./src/db.js');
-const port = process.env.PORT || 3001
-const cors = require("cors");
-const { addGenresToBasedata } = require('./src/controllers/getGenres.js');
+const server = require("./src/app.js");
+const { conn } = require("./src/db.js");
+const port = process.env.PORT || 3001;
+const axios = require("axios");
 
-server.use(cors())
+// the first time post "url"/genres (full the DB)
 // Syncing all the models at once.
-conn.sync({ force: true }).then(() => {
+conn.sync({ force: false }).then(() => {
   server.listen(port, async () => {
-    await addGenresToBasedata()
-    console.log(`server raised in port ${port}`); // eslint-disable-line no-console
+    await axios.post(`${process.env.DEPLOY_BACK_URL}/videogames/genres`);
+    console.log(`server raised in port ${port}`);
   });
 });
